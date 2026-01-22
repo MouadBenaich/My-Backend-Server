e041794a8b60ba94bf99439dbce114c62d5669f3
MouadBenaich <mobenchiso@gmail.com>
2026-01-22 16:44:03 +0100

    Update Clarifai endpoint with model version ID and logging

--- server.js
+++ server.js
@@ -16,17 +16,20 @@ app.post("/clarifai", async (req, res) => {
   });
 
   try {
-    const response = await fetch("https://api.clarifai.com/v2/models/face-detection/outputs", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Key " + process.env.CLARIFAI_KEY
-      },
-      body: raw
-    });
-
+    const response = await fetch(
+        "https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs",
+        {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: "Key " + process.env.CLARIFAI_KEY
+            },
+            body: raw
+        }
+    );
     const data = await response.json();
+    console.log("Clarifai API raw response:", JSON.stringify(data, null, 2));
     res.json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
